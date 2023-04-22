f17928249e96056709c809650447d1435c3482b2
benzratima0 <benzratima0@gmail.com>
2023-04-22 19:42:44 +0700

    first commit

---
+++ projectshopping.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2023 at 01:42 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projectshopping`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Firstname` varchar(255) NOT NULL,
+  `Lastname` varchar(255) NOT NULL,
+  `Gender` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `Email`, `Password`, `Firstname`, `Lastname`, `Gender`) VALUES
+(1, 'Benznapak0@gmail.com', 'Benznapak314140', 'ณภัค', 'ดีมงคล', 'หญิง'),
+(2, 'Thanatchaporn0@gmail.com', 'Thanatchaporn1234', 'ธนัชพร', 'ภาคาหาญ', 'หญิง'),
+(3, 'ParichatWeankeaw0@gmail.com', 'Parichat2545', 'Parichat', 'Weankeaw', 'หญิง'),
+(4, 'Tunwarat_1@gmail.com', 'Tunwarat0705', 'Tunwarat', 'Pimprasan', 'หญิง'),
+(5, 'Kanokwan0@gmail.com', 'Kanokwan1234', 'กนกวรรณ', 'สาครชาติ', 'หญิง'),
+(6, 'BenzNapak0@gmail.com', 'BenzNapak1234', 'Napak', 'Deemongkhon', 'หญิง');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ server.js
@@ -0,0 +1,78 @@
+var express = require('express')
+var cors = require('cors')
+const mysql = require('mysql2');
+require('dotenv').config()
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
+
+var app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/users', function (req, res, next) {
+connection.query(
+  'SELECT * FROM `users` ',
+  function(err, results, fields) {
+    res.json(results);
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+})
+
+app.get('/users', function (req, res, next) {
+  connection.query(
+      'SELECT * FROM `users`',
+      function (err, results, fields) {
+          res.json(results);
+      }
+  );
+})
+
+app.get('/users/:id', function (req, res, next) {
+  const id = req.params.id;
+  connection.query(
+      'SELECT * FROM `users` WHERE `id` = ?',
+      [id],
+      function (err, results) {
+          res.json(results);
+      }
+  );
+})
+
+app.post('/users', function (req, res, next) {
+  connection.query(
+      'INSERT INTO `users`(`Email`, `Password`, `Firstname`, `Lastname`, `Gender`) VALUES (?, ?, ?, ?, ?)',
+      [req.body.Email, req.body.Password, req.body.Firstname, req.body.Lastname, req.body.Gender],
+      function (err, results) {
+          res.json(results);
+          console.log(err)
+          console.log(results)
+      }
+  );
+})
+
+app.put('/users', function (req, res, next) {
+  connection.query(
+      'UPDATE `users` SET `Email`= ?, `Password`= ?, `Firstname`= ?, `Lastname`= ?, `Gender`= ? WHERE id = ?',
+      [req.body.Email, req.body.Password, req.body.Firstname, req.body.Lastname, req.body.Gender, req.body.id],
+      function (err, results) {
+          res.json(results);
+      }
+  );
+})
+
+app.delete('/users', function (req, res, next) {
+  connection.query(
+      'DELETE FROM `users` WHERE id = ?',
+      [req.body.id],
+      function (err, results) {
+          res.json(results);
+      }
+  );
+})
+
+app.listen(5000, function () {
+  console.log('CORS-enabled web server listening on port 5000')
+})
